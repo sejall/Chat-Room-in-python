02a75d41bda90d62bce88f4dada4063408d66058
sejall <33982474+sejall@users.noreply.github.com>
2018-08-12 07:52:56 +0530

    Add files via upload

---
+++ ChatRoom/Server.py
@@ -0,0 +1,66 @@
+import socket
+import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = socket.gethostbyname(socket.gethostname())
+port = 9995
+
+clients = {}
+
+s.bind((ip, port))
+s.listen(5)
+print('Server Ready...')
+
+def handleClient(client, uname):
+    clientConnected = True
+    keys = clients.keys()
+    help = 'There are four commands in Messenger\n1::chatlist=>gives you the list of the people currently online\n2::quit=>To end your session\n3::broadcast=>To broadcast your message to each and every person currently present online\n4::Add the name of the person at the end of your message preceded by @ to send it to particular person'
+
+    while clientConnected:
+        try:
+            msg = client.recv(1024).decode('ascii')
+            response = 'Number of People Online\n'
+            found = False
+            if 'chatlist' in msg:
+                clientNo = 0
+                for name in keys:
+                    clientNo += 1
+                    response = response + str(clientNo) + '::' + name + '\n'
+                client.send(response.encode('ascii'))
+            elif 'help' in msg:
+                client.send(help.encode('ascii'))
+            elif 'broadcast' in msg:
+                msg = msg.replace('broadcast', '')
+                for v in clients.values():
+                    v.send(msg.encode('ascii'))
+            elif 'quit' in msg:
+                response = 'Stopping Session and exiting...'
+                client.send(response.encode('ascii'))
+                clients.pop(uname)
+                print(uname + ' has been logged out')
+                clientConnected = False
+            else:
+                for name in keys:
+                    if ("@"+name) in msg:
+                        msg = msg.replace("@"+name, '')
+                        clients.get(name).send(msg.encode('ascii'))
+                        found = True
+                if not found:
+                    client.send('Trying to send message to invalid person.'.encode('ascii'))
+        except:
+            clients.pop(uname)
+            print(uname + ' has been logged out')
+            clientConnected = False
+
+
+while 1:
+    client, address = s.accept()
+    uname = client.recv(1024).decode('ascii')
+    print('%s connected to the server' % str(uname))
+    client.send('Welcome to friends for live!. Type help to know all the commands'.encode('ascii'))
+
+    if client not in clients:
+        clients[uname] = client
+        threading.Thread(target=handleClient, args=(client, uname,)).start()
+
+
